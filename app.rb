31721ee1f4544cf44abaa9a3194c144865baaa61
github username <github email address>
2019-09-16 15:51:04 -0400

    Done.

--- app.rb
+++ app.rb
@@ -20,5 +20,30 @@ class App < Sinatra::Base
     @number.to_i.times { test << @phrase }
     test
   end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+
+    @word1 + " " + @word2 + " " + @word3 + " " + @word4 + " " + @word5 + "."
+  end
   
+  get '/:operation/:number1/:number2' do 
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    @operation = params[:operation]
+    if @operation == "add"
+      (@number1 + @number2).to_s
+    elsif @operation == "subtract"
+      (@number1 - @number2).to_s
+    elsif @operation == "divide"
+      (@number1 / @number2).to_s
+    elsif @operation == "multiply"
+      (@number1 * @number2).to_s
+    end 
+  end
+
 end
